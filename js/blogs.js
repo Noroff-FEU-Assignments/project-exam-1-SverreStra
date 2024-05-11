7df28c2350be5affa563011c30865e0f06113233
SverreStra <114399609+SverreStra@users.noreply.github.com>
2024-05-11 15:00:17 +0200

    new test
    new test

--- js/blogs.js
+++ js/blogs.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://www.straumstra.no/wp-json/wp/v2/posts";
 const allPost = document.querySelector(".post-container");
-const olderPosts = document.querySelector(".older-posts");
+const leftPost = document.querySelector(".post-left");
+const rightPost = document.querySelector(".post-right");
 
 const getPosts = async () => {
   const response = await fetch(baseUrl);
@@ -23,4 +24,62 @@ function createPosts(posts) {
     <a href="blogspecific.html?id=${post.id}" class="read-more-btn btn">Read More</a>
     </div>`
   })
+};
+
+
+const leftPosts = async () => {
+  const response = await fetch("https://www.straumstra.no/wp-json/wp/v2/posts/167");
+  if (!response.ok) {
+    throw new Error (`HTTP Error! status ${response.status}`);
+  }
+  const data = await response.json();
+  postLeft(data);
+}
+
+leftPosts();
+
+function postLeft(posters) {
+   leftPost.innerHTML += `
+    <div class="left-posts">
+      <div class="card-image">
+        <img src="${posters.featured_media_src_url}" alt="${posters.title.rendered}" class="img">
+      </div>
+      <div class="card-content">
+        <h2>${posters.title.rendered}</h2>
+        <p class="phone-away">${posters.excerpt.rendered}</p>
+        <div class="button">
+          <a href="blogspecific.html?id=${posters.id}" class="read-more-btn btn">Read More</a>
+        </div>
+      </div>
+    </div>`
+  
+};
+
+
+const rightPosts = async () => {
+  const response = await fetch("https://www.straumstra.no/wp-json/wp/v2/posts/175");
+  if (!response.ok) {
+    throw new Error (`HTTP Error! status ${response.status}`);
+  }
+  const data = await response.json();
+  postRight(data);
+}
+
+rightPosts();
+
+function postRight(posters) {
+  rightPost.innerHTML += `
+    <div class="left-posts">
+      <div class="card-image">
+        <img src="${posters.featured_media_src_url}" alt="${posters.title.rendered}" class="img">
+      </div>
+      <div class="card-content">
+        <h2>${posters.title.rendered}</h2>
+        <p>${posters.excerpt.rendered}</p>
+        <div class="button">
+        <a href="blogspecific.html?id=${posters.id}" class="read-more-btn btn">Read More</a>
+        </div>
+      </div>
+    </div>`
+  
 };
