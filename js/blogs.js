2e0848b8d199aa92ec8c028fe142e8e675f5107a
SverreStra <114399609+SverreStra@users.noreply.github.com>
2024-05-11 15:18:22 +0200

    show update

--- js/blogs.js
+++ js/blogs.js
@@ -86,9 +86,9 @@ function postRight(posters) {
 
 function showMore() {
   var x = document.getElementById("two-posts");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 } 
